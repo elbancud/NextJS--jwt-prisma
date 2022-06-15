2c75fb98d02cef8af5307b27f990bc80d76a8117
emarson <elbancud@gmail.com>
2022-06-15 08:37:52 +0800

    fixed: tailwind module integration

--- src/pages/Client/Frontend/Core/Login.tsx
+++ src/pages/Client/Frontend/Core/Login.tsx
@@ -1,14 +1,8 @@
+import { LockClosedIcon } from "@heroicons/react/solid";
+
 export default function Login() {
 	return (
 		<>
-			{/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
 			<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 				<div className="max-w-md w-full space-y-8">
 					<div>
@@ -89,7 +83,12 @@ export default function Login() {
 								type="submit"
 								className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 							>
-								<span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+								<span className="absolute left-0 inset-y-0 flex items-center pl-3">
+									<LockClosedIcon
+										className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+										aria-hidden="true"
+									/>
+								</span>
 								Sign in
 							</button>
 						</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
-
 import { useAppDispatch, useAppSelector } from "../lib/custom-hooks";
 import { incremented, amountAdded } from "../lib/features/counter-slice";
-const Home: NextPage = () => {
-	const count = useAppSelector((state) => state.counter.value);
-	const dipatch = useAppDispatch();
+import Link from "next/link";
 
-	const handleIncrement = () => {
-		dipatch(amountAdded(3));
-	};
+const Home: NextPage = () => {
+	// const count = useAppSelector((state) => state.counter.value);
+	// const dipatch = useAppDispatch();
+
+	// const handleIncrement = () => {
+	// 	dipatch(amountAdded(3));
+	// };
 	return (
-		<div className={styles.container}>
-			{count}
-			<button onClick={handleIncrement}></button>
+		<div>
+			<Link href="./Client/Frontend/Core/Login">Admin</Link>
 		</div>
 	);
 };
