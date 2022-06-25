8fff3e52514da50e49f37c92323a117610476a3d
elbancud <elbancud@gmail.com>
2022-06-26 00:14:09 +0800

    chore: fetch from blog api scaffold| to fix - ssr props undefined

--- src/layouts/Screens/MainSection.tsx
+++ src/layouts/Screens/MainSection.tsx
@@ -1,4 +1,42 @@
-import react from "react";
-export default function MainSection() {
-	return <div>Main section</div>;
+import react, { useEffect, useState } from "react";
+import axiosClient from "../../lib/axios";
+
+function MainSection({ blogs }) {
+	return (
+		<div>
+			<ul>
+				{
+					blogs?.map(blog => {
+						return (
+							<li key={blog.id}>
+								{blogs.title} {blogs.content}
+							</li>	
+						)
+					})
+				}
+			</ul>
+		</div>
+	)
+
 }
+export async function getServerSideProps() {
+	const res = await fetch('api/queries/blogs');
+	const data = await res.json();
+	return {
+		props: {
+			blogs: data
+		}
+	}
+		// await axiosClient.get('/queries/blogs')
+		// 	.then(response => {
+		// 		return {
+		// 			props: {
+		// 				blogs: response.data
+		// 			}
+		// 		}
+		// 	}).catch(() => {
+				
+		// 	})
+		
+}
+export default MainSection;
